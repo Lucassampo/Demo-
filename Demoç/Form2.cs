e1ba9efaad4eca6222d949ceee1904dd1e9deae5
Alumnos <Alumnos@PC184>
2023-08-31 10:51:02 -0300

    Agregar archivos de proyecto.

---
+++ Demoç/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Demoç
+
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Directory.Exists("E:/Mosquito");
+            Directory.CreateDirectory("E:/Mosquito");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool valor = Directory.Exists("E:/Mosquito");
+            Directory.Delete("E:/Mosquito");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.Create("E:/Mosquito/Hola.txt");
+        }
+    }
+}
---
+++ Demoç/Problema.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demoç
+
+{
+    class Problema
+    {
+        public void saludar()
+        {
+            MessageBox.Show("Hola");
+        }
+    
+        public void verFrutas(ListBox lst)
+        {
+            string[] Frutas = { "UVA", "PERA", "LIMON", "MANZANA" , "POMELO" };
+            lst.Items.Clear();
+            foreach (string Fruta in Frutas) 
+            {
+                lst.Items.Add(Fruta);           
+            }
+            
+        }
+    }
+
+}
